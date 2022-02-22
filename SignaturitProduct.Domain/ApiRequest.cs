fb33fbe060b1a99f9e4922f7886f9e59ad539448
Antonio Arjona Torres <antonio-995@hotmail.com>
2022-02-22 23:21:37 +0100

    Domain elements created and dependencies set

---
+++ SignaturitProduct.Domain/ApiRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace SignaturitProduct.Domain
+{
+    public class ApiRequest
+    {
+        public ApiRequest() 
+        {
+            Plaintiff = "";
+            Defendant = "";
+        }
+
+        public string Plaintiff { get; set; }
+        public string Defendant { get; set; }
+    }
+}
